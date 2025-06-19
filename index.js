890228664b9076a9474b8c2d1f74732cc636755d
Phyoe Thiha <cybon149@gmail.com>
2025-06-19 21:40:08 +0630

    Creat game UI layout

---
+++ index.js
@@ -0,0 +1,11 @@
+// const container = document.querySelector(".container");
+// const CONTAINER_SIZE = 500;
+// let squareSize = CONTAINER_SIZE/16;
+//  for(let i = 0 ; i < 22 ; i++){
+//     const square = document.createElement("div");
+//     square.classList.add("square");
+//     square.style.width="16px"
+//     square.style.height="16px"
+//     square.style.backgroundColor="black";
+//     container.append(square);
+//  }
